4a42975b9189597eb207fc957c79ca6d2bbcf152
Camila Lagos <118062775+camilagos@users.noreply.github.com>
2025-04-28 23:00:37 -0400

    Arreglos url

--- payroll-frontend/src/http-common.js
+++ payroll-frontend/src/http-common.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 const payrollBackendServer = "karting-app.eastus2.cloudapp.azure.com";
+const payrollBackendPort = "8090";
 
 console.log(payrollBackendServer)
 console.log(payrollBackendPort)
 
 export default axios.create({
-    baseURL: `http://${payrollBackendServer}/api`,
+    baseURL: `http://${payrollBackendServer}:${payrollBackendPort}/api`,
     headers: {
         'Content-Type': 'application/json'
     }
