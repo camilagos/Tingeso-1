13b7cd5e2842d676e72d4c098574010f870f308f
Camila Lagos <118062775+camilagos@users.noreply.github.com>
2025-04-28 23:20:40 -0400

    Update http-common.js

--- payroll-frontend/src/http-common.js
+++ payroll-frontend/src/http-common.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
 const payrollBackendServer = import.meta.env.VITE_PAYROLL_BACKEND_SERVER;
-const payrollBackendPort = "8090";
 
 console.log(payrollBackendServer)
-console.log(payrollBackendPort)
+
 
 export default axios.create({
-    baseURL: `http://${payrollBackendServer}:${payrollBackendPort}/api`,
+    baseURL: `http://${payrollBackendServer}:8090/api`,
     headers: {
         'Content-Type': 'application/json'
     }
