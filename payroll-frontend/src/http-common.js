e455b7244bef441c61d74efbfd6cb473321bf24a
Camila Lagos <118062775+camilagos@users.noreply.github.com>
2025-04-28 22:31:13 -0400

    Cambio url

--- payroll-frontend/src/http-common.js
+++ payroll-frontend/src/http-common.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-const payrollBackendServer = import.meta.env.VITE_PAYROLL_BACKEND_SERVER;
-const payrollBackendPort = import.meta.env.VITE_PAYROLL_BACKEND_PORT;
+const payrollBackendServer = "karting-app.eastus2.cloudapp.azure.com";
 
 console.log(payrollBackendServer)
 console.log(payrollBackendPort)
 
 export default axios.create({
-    baseURL: `http://${payrollBackendServer}:${payrollBackendPort}/api`,
+    baseURL: `http://${payrollBackendServer}/api`,
     headers: {
         'Content-Type': 'application/json'
     }
